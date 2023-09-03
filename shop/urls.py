bfb6885a06bc4d57baa34fc826ab58bfc2d5c1cf
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-09-03 14:49:57 +0200

    Navbar

--- shop/urls.py
+++ shop/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from . import views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path("", include("shop.urls")),
+    path('', views.shop, name="shop"),
+    path('warenkorb/', views.warenkorb, name="warenkorb"),
+    path('kasse/', views.kasse, name="kasse"),
 ]
